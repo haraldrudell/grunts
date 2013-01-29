cde8d35c982ec414f59108a09774df8981e02a06
Harald Rudell <harald@therudells.com>
2013-01-28 19:21:13 -0800

    Ready for github

--- readme.md
+++ readme.md
@@ -1,1 +1,32 @@
 <h1>Grunts</h1>
+<p>Useful grunt tasks.</p>
+<p>&copy; 2013 [Harald Rudell](http://www.haraldrudell.com) wrote this for node in January, 2013</p>
+<h2>How to Use</h2>
+Add to your Gruntfile.js:
+```js
+	grunt.loadNpmTasks('grunts')
+```
+Add the project dependency to your project:
+```
+npm install grunts --save-dev
+```
+This inserts grunts into the devDependencies section of package.json
+Test the plugin:
+```
+$ grunt exports
+Running "exports" task
+>> No exports
+
+Done, without errors.
+```
+<h2>Task Reference</h2>
+<h3>grunt exports</h3>
+<p>Lists the exports if your module is a library.</p>
+<h3>grunt bump</h3>
+<p>Increases the third digit of the project's version number in package.json.</p>
+<p>also: grunt bump major, grunt bump minor</p>
+<h3>grunt test</h3>
+<p>Runs tests provided by the mochawrapper module.</p>
+<h2>Notes</h2>
+<p>&copy; 2013 [Harald Rudell](http://www.haraldrudell.com) wrote this for node in January, 2013</p>
+<p>Please suggest better ways, new features, and possible difficulties on <a  href=https://github.com/haraldrudell/haraldutil>github</a></p>
--- tasks/mocha.js
+++ tasks/mocha.js
@@ -1,27 +1,28 @@
 // mocha.js
-// Run all mocha tests
+// Run tests using mochawrapper module
 // © Harald Rudell 2013 <harald@allgoodapps.com> All rights reserved.
 
 // http://nodejs.org/api/path.html
 var path = require('path')
 var mochaPath = ['node_modules', 'mochawrapper', 'node_modules', 'mocha2']
+var options = {
+	reporter: 'spec',
+	ui: 'exports',
+}
 
 module.exports = function(grunt) {
 	grunt.registerTask('test', 'Run tests using mochawrapper module', doMocha)
 
 	function doMocha() {
-		var done = this.async()
+		var done = this.async() // task that ends with a callback
 
-		var options = {
-			reporter: 'spec',
-			ui: 'exports',
-		}
+		// top-level dependency mochawrapper, module mocha2, instantiate
 		var mocha2 = require(path.join.apply(path, [process.cwd()].concat(mochaPath)))
 		var mocha_instance = new mocha2(options)
 
-		mocha_instance.addFile('test/test-package.js')
+		grunt.file.expand('test/*.js').forEach(addFile) // add all tests to mocha
 
-		var mochaRunner
+		var mochaRunner // run tests
 		try {
 			// on syntax errors, mocha simply exits
 			mochaRunner = mocha_instance.run(mochaResult)
@@ -31,6 +32,10 @@ module.exports = function(grunt) {
 
 		function mochaResult(failureCount) {
 			done(failureCount === 0)
+		}
+
+		function addFile(file) {
+			mocha_instance.addFile(file)
 		}
 	}
 }
